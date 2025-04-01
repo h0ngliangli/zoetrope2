527a7b292c85e18abcde43e068c95d8f7574ff35
Hongliang Li <woodyli1982@gmail.com>
2025-04-01 14:44:20 -0700

    更新默认布局中的导航链接文本为中文

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,9 +2,9 @@
   <div class="flex flex-col h-screen">
     <header class="bg-gray-800 text-white p-1 flex gap-4 pl-4">
       <!-- <h1 class="text-2xl"></h1> -->
-      <ULink to="/add" as="button">Add</ULink>
-      <ULink to="/exec">Exec</ULink>
-      <ULink to="/search">Search</ULink>
+      <ULink to="/add" as="button">新建</ULink>
+      <ULink to="/exec">练习</ULink>
+      <ULink to="/search">查找</ULink>
       <span>前往</span>
       <UInput v-model="refGoto" @keydown.enter="onEdit" />
     </header>
