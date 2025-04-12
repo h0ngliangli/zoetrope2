a62209de992646f8772388059a5599f6ff12730b
Hongliang Li <woodyli1982@gmail.com>
2025-04-12 14:47:13 -0700

    init

---
+++ app/pages/test/test-hash.vue
@@ -0,0 +1,22 @@
+<template>
+  <pre
+    >{{ refMainString }}
+  f</pre>
+</template>
+
+<script setup>
+const route = useRoute()
+const refMain = ref({
+  fullPath: route.fullPath,
+  hash: route.hash,
+  query: route.query,
+  matched: route.matched,
+  meta: route.meta,
+  name: route.name,
+  path: route.path,
+  redirectedFrom: route.redirectedFrom,
+})
+const refMainString = computed(() => {
+  return JSON.stringify(refMain.value, null, 2)
+})
+</script>
