61b45299c02206beebc5e383c74df6ffa2c781ef
Hongliang Li <woodyli1982@gmail.com>
2025-04-15 19:51:25 -0700

    添加Prettier配置文件

---
+++ .prettierrc.mjs
@@ -0,0 +1,24 @@
+export default {
+  // 是否在语句末尾添加分号
+  semi: false,
+  // 使用单引号而不是双引号
+  singleQuote: true,
+  // 缩进使用的空格数
+  tabWidth: 2,
+  // 是否使用 tab 缩进
+  useTabs: false,
+  // 对象、数组等最后一项是否添加逗号
+  trailingComma: 'es5',
+  // 一行最大字符数，超过会自动换行
+  printWidth: 100,
+  // 对象字面量的括号之间是否需要空格
+  bracketSpacing: true,
+  // 箭头函数参数是否总是使用括号
+  arrowParens: 'always',
+  // 换行符类型
+  endOfLine: 'lf',
+  // Vue 文件中的 script 和 style 标签内是否缩进
+  vueIndentScriptAndStyle: true,
+  // HTML 空白符敏感度
+  htmlWhitespaceSensitivity: 'css'
+}
